129f959b95b842e7a3c4920ed5ba1be9fdcb3025
Matthew Cale <mattc41190@gmail.com>
2016-05-19 22:29:25 -0500

    removed commented code

--- spec/main/constructor_spec.js
+++ spec/main/constructor_spec.js
@@ -1,6 +1,7 @@
 var Car = require('../../patterns/general/creational/constructor.js');
 
 describe('Car objects', function(){
+  
   it('should be able to make a Car', function(){
     var ram = new Car('Dodge Ram', 1999, 200000);
     expect(ram.model).toBe('Dodge Ram');
@@ -10,9 +11,5 @@ describe('Car objects', function(){
     var ram = new Car('Dodge Ram', 1999, 200000);
     expect(ram.toString()).toEqual("Hello Matt");
   });
-  //
-  // it("can tell who you are saying hello to", function() {
-  //   expect(Salutations.greet("Brian")).not.toEqual("Hello Matt");
-  // });
 
 });
